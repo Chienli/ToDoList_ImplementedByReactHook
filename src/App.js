bcecfe8529a0a365142204aba5f731869b5e707a
Guy Lin Chien Li <guy.lin@titansoft.com.sg>
2019-08-29 15:55:03 +0800

    WIP

--- src/App.js
+++ src/App.js
@@ -1,26 +1,81 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import styled from "styled-components";
+import Task_Modal from "./Task_Modal";
+const Container = styled.div`
+  background-color: #eeeeee;
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: flex-start;
+  flex-direction: column;
+  position: absolute;
+  z-index: -1;
+`;
 
-function App() {
+const Title = styled.div`
+  font-family: consolas;
+  font-weight: bold;
+  font-size: 64px;
+  color: #00b5ad;
+`;
+
+const Add_Task = styled.button`
+  width: 70%;
+  height: 45px;
+  font-family: consolas;
+  font-weight: bold;
+  font-size: 32px;
+  color: #ffffff;
+  background-color: #00b5ad;
+  border: none;
+  outline: none;
+  box-shadow: rgba(0, 0, 0, 0.3) 0 0 6px;
+  :active {
+    box-shadow: none;
+  }
+`;
+
+const App = () => {
+  const [isModalActice, setIsModalActive] = useState(false);
+  const [task, setTask] = useState({
+    taskName: "",
+    description: "",
+    timestamp: new Date().getTime()
+  });
+  const [tasks, setTasks] = useState([]);
+  function handleIsModalActiveChange() {
+    setIsModalActive(prevState => !prevState);
+  }
+  function handleTaskFormChange(e) {
+    const { name, value } = e.target;
+    const newTask = { ...task };
+    newTask[name] = value;
+    setTask(prevState => (prevState = newTask));
+  }
+  function addTask() {
+    const newTask = { ...task };
+    newTask.timestamp = new Date().getTime();
+    setTask(prevState => (prevState = newTask));
+    const newTasks = [...tasks];
+    newTasks.push(task);
+    setTasks(prevState => (prevState = newTasks));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Container>
+        <Title>TO DO LIST</Title>
+        <Add_Task onClick={handleIsModalActiveChange}>New Task</Add_Task>
+      </Container>
+      {isModalActice && (
+        <Task_Modal
+          handleTaskFormChange={handleTaskFormChange}
+          handleIsModalActiveChange={handleIsModalActiveChange}
+          addTask={addTask}
+        />
+      )}
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/Task_Modal.js
@@ -0,0 +1,125 @@
+import React from "react";
+import styled from "styled-components";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  background-color: #000000;
+  opacity: 0.7;
+  position: absolute;
+  z-index: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
+
+const CardContainer = styled.div`
+  width: 450px;
+  height: 250px;
+  background-color: #fff;
+  border-radius: 5px;
+`;
+const CardHeader = styled.div`
+  width: 450px;
+  height: 40px;
+  background-color: #00b5ad;
+  border-radius: 5px 5px 0 0;
+  font-family: consolas;
+  text-align: center;
+  line-height: 40px;
+  color: #ffffff;
+  font-weight: bold;
+  font-size: 20px;
+`;
+const CardBody = styled.div`
+  width: 450px;
+  height: 170px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-around;
+  input {
+    width: 300px;
+    height: 40px;
+    border-radius: 5px;
+    border: 1px solid rgba(0, 0, 0, 0.6);
+    padding-left: 10px;
+    outline: none;
+    font-family: "consolas", "microsoft jhenghei";
+    ::placeholder {
+      font-family: consolas;
+    }
+    :focus {
+      box-shadow: rgba(0, 0, 0, 0.6) 0 0 6px;
+    }
+  }
+`;
+
+const CardFooter = styled.div`
+  width: 450px;
+  height: 40px;
+  border-radius: 0 0 5px 5px;
+  font-family: consolas;
+  text-align: center;
+  line-height: 40px;
+  color: #ffffff;
+  font-weight: bold;
+  font-size: 20px;
+`;
+
+const Button = styled.button`
+  width: 225px;
+  height: 40px;
+  outline: none;
+  border: none;
+  color: white;
+  font-size: 18px;
+  font-family: consolas;
+  border-radius: ${props =>
+    props.className === "cancel" ? "0 0 0 5px" : "0 0 5px 0"};
+  background-color: ${props =>
+    props.className === "cancel" ? "red" : "green"};
+  :hover {
+    background-color: #fff;
+    color: ${props => (props.className === "cancel" ? "red" : "green")};
+    border-width: ${props =>
+      props.className === "cancel" ? "1px 1px 1px 0" : "1px 0 1px 1px"};
+    border-style: solid;
+    border-color: ${props => (props.className === "cancel" ? "red" : "green")};
+  }
+`;
+
+const Task_Modal = props => {
+  const { handleTaskFormChange, handleIsModalActiveChange, addTask } = props;
+  return (
+    <Container>
+      <CardContainer>
+        <CardHeader>New Task</CardHeader>
+        <CardBody>
+          <input
+            onChange={handleTaskFormChange}
+            type="text"
+            placeholder="Task Name"
+            name="taskName"
+          />
+          <input
+            onChange={handleTaskFormChange}
+            type="text"
+            placeholder="Description"
+            name="description"
+          />
+        </CardBody>
+        <CardFooter>
+          <Button onClick={handleIsModalActiveChange} className="cancel">
+            Cancel
+          </Button>
+          <Button onClick={addTask} className="add">
+            Add
+          </Button>
+        </CardFooter>
+      </CardContainer>
+    </Container>
+  );
+};
+
+export default Task_Modal;
